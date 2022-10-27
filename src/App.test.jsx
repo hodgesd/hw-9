6bfbe82267d4e2653fedc29a0516c2cc9d28ade6
Manav Misra <manavm@visionwebsoft.com>
2022-10-27 10:38:30 -0500

    test: :white_check_mark: change 'test' to 'it'

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders Tic Tac Toe", () => {
+it("renders Tic Tac Toe", () => {
   render(<App />);
 
   const squares = screen.getAllByRole("button");
